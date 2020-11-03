ae56d4ba84f35e0957f3ddd0e0e29c878e6b6718
Aurélien Lambert <aure@olli-ai.com>
2020-11-03 10:38:55 +0700

    update tag too

--- scripts/bump-app-version.sh
+++ scripts/bump-app-version.sh
@@ -51,10 +51,13 @@ git commit -m "Bump appVersion to 'v${RELEASE_VERSION}'"
 ## rebase
 git pull --rebase publisher master
 
+## update the tag
+git tag -f "v${RELEASE_VERSION}"
+
 if [[ "${1}" == "publish" ]]; then
 
     ## publish changes
-    git push publisher master
+    git push publisher master "v${RELEASE_VERSION}"
 
     ## trigger helm-charts reload
     curl -X POST "https://api.github.com/repos/${CHARTS_REPOSITORY}/dispatches" -u "${RELEASE_USER}:${RELEASE_USER_TOKEN}" -d '{"event_type": "updateCharts"}'
